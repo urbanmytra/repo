5e5c6689fca110e30be0c46ef7d3a0f9d4671286
Aritra Mukherjee <aritramukherjee1509@gmail.com>
2026-01-22 01:47:48 +0530

    api change in admin

--- Web/admin/src/utils/api.js
+++ Web/admin/src/utils/api.js
@@ -1,6 +1,6 @@
 // API Configuration and HTTP client
 // const API_BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1';
-const API_BASE_URL = 'https://project-bagajation-backend.onrender.com/api/v1';
+const API_BASE_URL = 'https://urbanmytrabackend.onrender.com/api/v1';
 
 
 class ApiClient {
